c8bbb8f831c8671eb550e98800c12e090553f187
Scorpius <el-zaiat@msn.com>
2021-09-12 14:25:21 +0200

    Add rate-limit support

--- src/main/java/com/scorpius/blockchain/bitcoin/BTCExplorer.java
+++ src/main/java/com/scorpius/blockchain/bitcoin/BTCExplorer.java
@@ -1,21 +1,54 @@
 package com.scorpius.blockchain.bitcoin;
 
 import dev.yasper.rump.Rump;
-import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.Callable;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import lombok.Getter;
+import lombok.Setter;
 
-// TODO: Respect blockchain.info's ratelimits, 1 request every 10 seconds
 public class BTCExplorer {
 
     private static final String BASE = "https://blockchain.info/";
     private static final String SINGLE_TRANSACTION = BASE + "rawtx/";
     private static final String SINGLE_ADDRESS = BASE + "rawaddr/";
 
-    // TODO: Get all transactions, currently limited to 50 by blockchain.info
-    public BTCAddress getAddress(String address) throws IOException {
-        return Rump.get(SINGLE_ADDRESS + address, BTCAddress.class).getBody();
+    private final Lock lock;
+
+    @Getter
+    @Setter
+    private Duration timePerRequest;
+    private Instant lastRequestInstant;
+
+    public BTCExplorer() {
+        this.lock = new ReentrantLock(true);
+        this.timePerRequest = Duration.ofSeconds(10);
+        this.lastRequestInstant = Instant.now().minus(timePerRequest);
     }
 
-    public BTCTransaction getTransaction(String hash) throws IOException {
-        return Rump.get(SINGLE_TRANSACTION + hash, BTCTransaction.class).getBody();
+    // TODO: Get all transactions, currently limited to 50 by blockchain.info
+    public BTCAddress getAddress(String address) throws Exception {
+        return process(() -> Rump.get(SINGLE_ADDRESS + address, BTCAddress.class).getBody());
+    }
+
+    public BTCTransaction getTransaction(String hash) throws Exception {
+        return process(() -> Rump.get(SINGLE_TRANSACTION + hash, BTCTransaction.class).getBody());
+    }
+
+    private <T> T process(Callable<T> callable) throws Exception {
+        if (lastRequestInstant.plus(timePerRequest).isBefore(Instant.now())) {
+            try {
+                lock.lockInterruptibly();
+                return callable.call();
+            } finally {
+                lastRequestInstant = Instant.now();
+                lock.unlock();
+            }
+        } else {
+            Thread.sleep(1);
+            return process(callable);
+        }
     }
 }
